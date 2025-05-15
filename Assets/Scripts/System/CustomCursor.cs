4b2f94849347640d3230525bf8f0b4365552ee84
celilsens <celilsens34@gmail.com>
2025-05-16 01:05:05 +0300

    v0.85 Cursor Destroy Fix

--- Assets/Scripts/System/CustomCursor.cs
+++ Assets/Scripts/System/CustomCursor.cs
@@ -7,7 +7,6 @@ public class CustomCursor : MonoBehaviour
 
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
         Cursor.visible = false;
         _cursorRectTransform = GetComponent<RectTransform>();
     }
